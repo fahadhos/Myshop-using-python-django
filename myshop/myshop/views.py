3c591dd7667146c64f0b1f363ca8d49a6de56293
fahadhos <generalus159@gmail.com>
2022-05-15 19:22:12 +0600

    For adding function to view in webpage

--- myshop/myshop/views.py
+++ myshop/myshop/views.py
@@ -1,7 +1,31 @@
-
 from django.http import HttpResponse
+
+from django.shortcuts import render
 
 
 def index(request):
-   return HttpResponse("hello")
-   
+    return render(request, 'Easy_buy/index.html')
+
+
+def about(request):
+    return HttpResponse("THis is about")
+
+
+def search(request):
+    return HttpResponse("THis is search bar")
+
+
+def contact(request):
+    return HttpResponse("THis is contact")
+
+
+def tracker(request):
+    return HttpResponse("THis is trackr")
+
+
+def checkout(request):
+    return HttpResponse("THis is checkout")
+
+
+def productview(request):
+    return HttpResponse('this is product view')
